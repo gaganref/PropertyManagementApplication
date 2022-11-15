ec3bda11d61f7ca22fdc9c71176a72bc711dd1a8
Varshini <polasanivarshini99@gmail.com>
2022-11-15 14:43:17 +0000

    added new classes

--- src/main/java/com/pma/web/repository/TenantRepository.java
+++ src/main/java/com/pma/web/repository/TenantRepository.java
@@ -1,7 +1,9 @@
 package com.pma.web.repository;
 
+import com.pma.web.model.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TenantRepository {
+public interface TenantRepository extends JpaRepository<Tenant, Long> {
 }
